89261e9640b424d7cfcb4fca2a370bd505e142ab
nishantrajput1 <nishant.rajput0502@gmail.com>
2026-01-12 18:12:50 +0530

    Mount and unmount and more

--- components/User.js
+++ components/User.js
@@ -3,15 +3,10 @@ const User = (props)=>{
     const [count,setcount] = useState(0);
     const [anothercount,setanothercount] = useState(2);
     const {name} = props;
+    
     return (
         <div className="user-card"> 
-            <button onClick={()=>{setcount(count+1)}}>+</button>
-            <button onClick={()=>{setcount(count-1)}}>-</button>
-            <h1>{`Count : ${count}`}</h1>
-            <h1>{`Another Count : ${anothercount}`}</h1>
-            <h2>Name : {name}</h2>
-            <h3>Location : Dehradun</h3>
-            <h4>Contact : @nishantrajput1</h4>
+            <h1>Hello i am the new way </h1> 
         </div>
     )
 }
--- components/UserClass.js
+++ components/UserClass.js
@@ -4,45 +4,59 @@ class UserClass extends React.Component
     constructor(props){
         super(props);
         console.log(props);
-        this.state ={
-            count : 0,
-            anothercount : 2,
-        };
+        this.state = {
+            userInfo : {
+                name : "Dummy",
+                location : "Default-location",
+                avatar_url : "I am the dummy avatar"
+            }
+        }
         console.log("Child Constructor is Called");
         
-        // this.setState(n){
-        //     count : n;
-        // };
-        // best place to create state variables 
+         // best place to create state variables 
     }
-    componentDidMount(){
+
+    async componentDidMount(){
         console.log("Component mounted successfully");
-        
+        const data = await fetch("https://api.github.com/users/nishantrajput1");
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            userInfo : json,
+        })
     }
-    // state variable tab initialize hota tha jab instance of Userclass 
-    // banta tha 
-    // old way of creating state
+    componentDidUpdate(){
+        console.log("Component Did Update");
+    }
     render(){
-        console.log("Render Function Called");
-        
-        const {name,location} = this.props;
+        const {name , bio,avatar_url} = this.state.userInfo
+        console.log("Component is rendered")
         return (
             <div className="user-card">
-                <button onClick={()=>{
-                    this.setState({
-                        count : this.state.count+1,
-                    })
-                    }}>
-                        +
-                </button>
-                <button onClick={()=>{this.setState({count : this.state.count-1})}}>-</button>
-            
-                <h1>Count : {this.state.count}</h1>
-                <h2>Name : {this.props.name}</h2>
-                <h3>Location : {this.props.location}</h3>
+                <img src={avatar_url} alt="" />
+                <h2>Name : {name}</h2>
+                <h3>bio : {bio}</h3>
                 <h4>Contact : @nishantrajput1</h4>
             </div>
         )
     }
 }
 export default UserClass;
+
+//  Constructor is called
+//  Render (dummy)
+//  <HTML Dummy>
+//  Component Did Mount
+//  < API Call >
+//  < this.setState> state variable i update
+// (It triggers the reconciliation process and the update cycle )
+// --- Mounting cycle finished
+// ------ Update cycle starst
+//  ---- render(API Data)
+// <HTML (new API data)>
+// componentDidUpdate
+// 
+// 
+// 
+// 
+// 
